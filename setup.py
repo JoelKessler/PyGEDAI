7c5d337369894e735255fd3bb16b939afe8b7dc6
Joel Kessler <joelkessler1412@gmail.com>
2025-11-07 23:15:29 +0100

    torch req

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="PyGEDAI",
+    name="pygedai",
     version="0.1.0",
     author="Joel Kessler",
     packages=find_packages(),
     install_requires=[
-        "torch>=2.8.0",
-    ]
+        # pure-Python deps here, but NOT torch
+    ],
+    extras_require={
+        # do: pip install pygedai[torch]
+        "torch": ["torch>=2.2.0"], # no CUDA channel here, choose at install time
+    },
 )
